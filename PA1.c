226d7f02bf6ce3eba12b627dbfa74e7a3446ba9b
AryanSingh-03 <6800aryansingh03@gmail.com>
2024-07-01 11:35:45 +0530

    Add files via upload

---
+++ PA1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int  mian ()
+{
+	float  fahrenheit,degree , celsius ;
+	printf("enter celsius :");
+	scanf("%f,& celsius");
+	
+    printf("fahrenheit=°F = (9/5 * °C) + 32");
+
+	printf("fahrenheit :%f%f,fahrenheit");
+																	
+																	
+}
---
+++ PR1.2.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main ()
+{
+		float basesalary , grosssalary, HRA,DA,TA ;
+		basesalary=100;
+		HRA=10;
+		DA=5;
+		TA=8; 
+		
+		 printf (" Enter your base salary");
+		 scanf  ("%f , & Base salary" );
+		 
+		printf("Gross salary = %d", basesalary*10/100);
+		 
+ } 
