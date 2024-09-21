b205ed88d422344a6eebf8161d11574b404e3264
AryanSingh-03 <6800aryansingh03@gmail.com>
2024-09-21 10:02:34 +0530

    Add files via upload

---
+++ c language final exam/Q2 pattern.c.c
@@ -0,0 +1,16 @@
+#include<stdio.h> 
+#include<conio.h>
+// write below  pattern using nested loop 
+main ()
+{
+	int i , j;
+	
+	for (i=45; i<=49;i++)
+	{
+		for (j=i;j<=49;j++)
+		{
+		    	printf("%d ",j);
+		    	
+		}printf("\n");
+	} 	
+}
---
+++ c language final exam/Q5.c.c
@@ -0,0 +1,18 @@
+#include<stdio.h> 
+#include<conio.h>
+// write a C program to find the  sum of all the elements  of the array 
+#include<stdio.h>
+#include<conio.h>
+
+int  main ()
+{
+	float  fahrenheit,degree , celsius ;
+	printf("enter celsius :");
+	scanf("%f,& celsius");
+	
+    printf("fahrenheit=°F = (9/5 * °C) + 32");
+
+	printf("fahrenheit :%f%f,fahrenheit");
+																	
+																	
+}
